51c7e4a16078d9a84e813932b38c55e9f2e42b54
Aviv Biton <avivbiton01@gmail.com>
2019-03-20 23:16:41 +0200

    Added bable and jest support

---
+++ src/utils.test.ts
@@ -0,0 +1,9 @@
+import { randomNumber } from "./utils";
+
+test("randomNumber - max random number is exclusive", () => {
+	expect(randomNumber(0, 1)).toBe(0);
+});
+
+test("randomNumber - number is not higher than min", () => {
+	expect(randomNumber(1, 2)).not.toBeGreaterThan(1);
+});

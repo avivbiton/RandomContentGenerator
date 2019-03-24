d4d531f4c78b9b23f43a62a8e7012a5fbad57991
Aviv Biton <avivbiton01@gmail.com>
2019-03-24 20:16:39 +0200

    Updated package.json and readme

--- index.ts
+++
@@ -1,5 +0,0 @@
-import { ContentGenerator } from "./src/ContentGenerator";
-import exampleSchema from "./exampleSchema.json";
-
-let gen = new ContentGenerator(exampleSchema);
-console.log(gen.build());

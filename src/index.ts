a7bc31a7e1c1fd25d69f8cf5e8753d7e717ce7ed
Aviv Biton <avivbiton01@gmail.com>
2019-10-02 13:07:01 +0300

    Add index.ts as main export file

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { Schema } from './Schema';
+export { Field } from './Field';
+export { ContentGenerator } from "./ContentGenerator";
+
+export { Parser } from './Parsers/Parser';
+export { BasicParser } from "./Parsers/BasicParser";
+export { MinMaxParser } from "./Parsers/MinMaxParser";
+export { MultiPickerParser } from './Parsers/MultiPickerParser';
